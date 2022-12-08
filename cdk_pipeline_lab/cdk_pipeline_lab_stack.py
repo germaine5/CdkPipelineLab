1bf52f7374f9f0d49b06f655221ef0b0e788af13
EC2 Default User <ec2-user@ip-172-31-26-15.ec2.internal>
2022-12-08 06:50:28 +0000

    second commit

--- cdk_pipeline_lab/cdk_pipeline_lab_stack.py
+++ cdk_pipeline_lab/cdk_pipeline_lab_stack.py
@@ -3,17 +3,35 @@ from aws_cdk import (
     Stack,
     # aws_sqs as sqs,
 )
+import aws_cdk as cdk
 from constructs import Construct
+from aws_cdk import aws_codepipeline as codepipeline
+from aws_cdk import aws_codepipeline_actions as cpactions
+from aws_cdk import pipelines
+from aws_cdk import codepipelines
+from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep, ManualApprovalStep
+from cdk_pipeline_lab.my_pipeline_app_stage import MyPipelineAppStage
 
 class CdkPipelineLabStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        pipeline =  CodePipeline(self, "Pipeline", 
+                        pipeline_name="MyPipeline",
+                        synth=ShellStep("Synth", 
+                            input=CodePipelineSource.git_hub("germaine5/CdkPipelineLab", "main"),
+                            
+                            commands=["npm install -g aws-cdk", 
+                                "python -m pip install -r requirements.txt", 
+                                "cdk synth"]
+                        )
+                    )
+                    
+        pipeline.add_stage(MyPipelineAppStage(self, "test",
+            env=cdk.Environment(account="915879673089", region="us-east-1")))
+            
+        testing_stage = pipeline.add_stage(MyPipelineAppStage(self, "testing",
+            env=cdk.Environment(account="915879673089", region="us-east-1")))
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkPipelineLabQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        testing_stage.add_post(ManualApprovalStep('approval'))
